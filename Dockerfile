f0ac47329f2380359843a0e96600b3926277364c
ravi <raravikumar432@gmail.com>
2023-07-28 09:36:25 +0530

    Docker update

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY  package.json /app
 RUN npm install
 COPY . /app
 CMD node server.js
-EXPOSE 0303
+EXPOSE 2001
--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.get('/hello', function (req, res) {
     res.send(person);
 });
 
-var server = app.listen(0303, function () {
+var server = app.listen(2001, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://localhost:0303", host, port)
+    console.log("Example app listening at http://localhost:2001", host, port)
 });
 
   
